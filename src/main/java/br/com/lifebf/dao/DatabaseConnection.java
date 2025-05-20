1bb18a9aa4b3df29c33f53481cdda87eda8d690f
Bruno Rosa <brunorosa5sr@gmail.com>
2025-05-19 23:16:31 -0300

    deploy1

--- src/main/java/br/com/lifebf/dao/DatabaseConnection.java
+++ src/main/java/br/com/lifebf/dao/DatabaseConnection.java
@@ -5,15 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/lifebf";
+    private static final String URL = "jdbc:mysql://root:KqIZWhJMuAfPHADOPZiWDOImxAfFOLVT@shinkansen.proxy.rlwy.net:37862/lifebf";
     private static final String USER = "root";
-    private static final String PASSWORD = "Admin@local";
-
+    private static final String PASSWORD = "KqIZWhJMuAfPHADOPZiWDOImxAfFOLVT";
 
     public static Connection getConnection() {
         try {
+            // 🔧 Importante: carregar explicitamente o driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
-            } catch (SQLException e) {
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver JDBC do MySQL não encontrado!", e);
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao conectar com o banco de dados", e);
         }
     }
